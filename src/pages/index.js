5ca16ab86c64ad0554ffd36d6c938dc18c2791a5
Prakash Kumar <prakash.kumar@active.ai>
2020-05-24 09:46:48 +0530

    responsiveness for small devices

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,28 @@
 import React from "react"
 import Layout from "../components/layout"
-import { Row, Col, Hidden } from "react-grid-system"
+import { Row, Col, Hidden, Visible } from "react-grid-system"
 import { Person } from "../icons/index"
 import Introduction from "../components/Introduction"
 
 export default () => (
   <Layout>
     <Row>
-      <Col sm={7}>
-        <Introduction />
-      </Col>
-      <Col offset={{ sm: 1 }} align="end" sm={4}>
-        <Hidden xs sm>
+      <Visible xs sm>
+        <Col align="center" sm={12} xs={12}>
           <Person className="person"></Person>
-        </Hidden>
-      </Col>
+        </Col>
+        <Col offset={{ md: 1 }} sm={12} xs={12}>
+          <Introduction />
+        </Col>
+      </Visible>
+      <Hidden xs sm>
+        <Col lg={7} md={7}>
+          <Introduction />
+        </Col>
+        <Col offset={{ lg: 1 }} md={4} align="end" lg={4}>
+          <Person className="person"></Person>
+        </Col>
+      </Hidden>
     </Row>
   </Layout>
 )
